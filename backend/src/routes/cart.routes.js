6d616499bf9cded1546df6f316729751abc19d11
Dinesh Samantaray <dineshsamantra0@gmail.com>
2026-07-22 02:02:29 +0530

    feat(cart): add cart routes

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -2,13 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require("../middleware/auth.middleware");
+const validate = require("../middleware/validation.middleware");
 
 const {
     addToCart,
     getCart,
 } = require("../controllers/cart.controller");
 
-router.post("/", protect, addToCart);
-router.get("/", protect, getCart);
+const {
+    addToCartValidator,
+} = require("../validators/cart.validator");
+
+// =====================
+// User Cart Routes
+// =====================
+
+// Add Product to Cart
+router.post(
+    "/",
+    protect,
+    validate(addToCartValidator),
+    addToCart
+);
+
+// Get Logged-in User Cart
+router.get(
+    "/",
+    protect,
+    getCart
+);
 
 module.exports = router;
